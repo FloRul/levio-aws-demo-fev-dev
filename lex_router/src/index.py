b42a8d3007578ee3d8f6e788fe68848ac9f5f50c
FloRul <rumiel.florian@gmail.com>
2024-02-02 14:22:50 -0500

    Fix intent resolution issue in router function

--- lex_router/src/index.py
+++ lex_router/src/index.py
@@ -11,15 +11,15 @@ def router(event):
         intent_name = event["sessionState"]["intent"]["name"]
         fn_name = os.environ.get(intent_name)
         print(f"Intent: {intent_name} -> Lambda: {fn_name}")
-        if fn_name:
+        if not fn_name:
+            raise ValueError(f"Intent {intent_name} could not be resolved")
+        else:
             invoke_response = client.invoke(
                 FunctionName=fn_name, Payload=json.dumps(event)
             )
             print(f"invoke respoonse : {invoke_response}")
             payload = json.load(invoke_response["Payload"])
             return payload
-        else:
-            raise ValueError(f"Intent {intent_name} could not be resolved")
     except Exception as e:
         print(f"Error during intent lambda routing : {e}")
         raise e
