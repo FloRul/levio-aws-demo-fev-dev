0c56dcfc96ba7af84c3c404b87efd01095bbbb54
FloRul <rumiel.florian@gmail.com>
2024-02-02 16:22:01 -0500

    Refactor database connection and reinitialize if closed

--- list_collections/src/index.py
+++ list_collections/src/index.py
@@ -1,40 +1,42 @@
 import os
 import psycopg2
 
-# from aws_lambda_powertools.utilities import parameters
-
 PGVECTOR_HOST = os.environ.get("PGVECTOR_HOST", "localhost")
 PGVECTOR_PORT = int(os.environ.get("PGVECTOR_PORT", 5432))
 PGVECTOR_DATABASE = os.environ.get("PGVECTOR_DATABASE", "postgres")
 PGVECTOR_USER = os.environ.get("PGVECTOR_USER", "postgres")
-# PG_PASSWORD = parameters.get_secret(
-#     os.environ.get("PG_PASSWORD_SECRET_NAME", "pg-password")
-# )
-
 COLLECTION_TABLE_NAME = "langchain_pg_collection"
+
+# Initialize the connection outside of the handler
+conn = psycopg2.connect(
+    dbname=PGVECTOR_DATABASE,
+    user=PGVECTOR_USER,
+    password="dbreader",  # read only account
+    host=PGVECTOR_HOST,
+    port=PGVECTOR_PORT,
+)
 
 
 def lambda_handler(event, context):
     rows = []
     try:
-        # Connect to the database
-        with psycopg2.connect(
-            dbname=PGVECTOR_DATABASE,
-            user=PGVECTOR_USER,
-            password="dbreader",  # read only account
-            host=PGVECTOR_HOST,
-            port=PGVECTOR_PORT,
-        ) as conn:
-            with conn.cursor() as cur:
-                # Execute a query
-                cur.execute(f"SELECT name FROM {COLLECTION_TABLE_NAME}")
+        # Check if the connection is still open
+        if conn.closed:
+            # If it's closed, reinitialize it
+            conn = psycopg2.connect(
+                dbname=PGVECTOR_DATABASE,
+                user=PGVECTOR_USER,
+                password="dbreader",  # read only account
+                host=PGVECTOR_HOST,
+                port=PGVECTOR_PORT,
+            )
 
-                # Fetch the results
-                rows = cur.fetchall()
+        with conn.cursor() as cur:
+            # Execute a query
+            cur.execute(f"SELECT name FROM {COLLECTION_TABLE_NAME}")
 
-                # Print the results
-                for row in rows:
-                    rows.append(row)
+            # Fetch the results
+            rows = cur.fetchall()
 
         return rows
     except Exception as e:
