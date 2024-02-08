53f1228c6b1001aba0cc7f74f424d4f9f0e7ea60
brunoabreu-levio <bruno.abreu@levio.ca>
2024-02-08 16:41:14 -0500

    Fix api_gateway module

--- terraform/modules/api_gateway/main.tf
+++ terraform/modules/api_gateway/main.tf
@@ -27,7 +27,7 @@ resource "aws_api_gateway_integration" "api_gateway_integration" {
   rest_api_id             = aws_api_gateway_rest_api.api_gateway_rest_api.id
   resource_id             = aws_api_gateway_resource.api_gateway_resource.id
   http_method             = aws_api_gateway_method.api_gateway_method.http_method
-  integration_http_method = var.api_gateway_http_method
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.lambda_function_arn}/invocations"
 }
