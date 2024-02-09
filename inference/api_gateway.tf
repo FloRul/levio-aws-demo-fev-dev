0536c8b888264bdeffdb20d6be60cec53bd526fb
FloRul <rumiel.florian@gmail.com>
2024-02-09 14:51:00 -0500

    Refactor API Gateway configuration

--- inference/api_gateway.tf
+++ inference/api_gateway.tf
@@ -1,14 +1,14 @@
-locals {
-  api_gateway_path_part   = "inference"
-  api_gateway_http_method = "GET"
-  api_gateway_stage_name  = "dev"
-}
+# locals {
+#   api_gateway_path_part   = "inference"
+#   api_gateway_http_method = "GET"
+#   api_gateway_stage_name  = "dev"
+# }
 
-module "api_gateway" {
-  source                  = "../terraform/modules/api_gateway"
-  api_gateway_path_part   = local.api_gateway_path_part
-  api_gateway_http_method = local.api_gateway_http_method
-  api_gateway_stage_name  = local.api_gateway_stage_name
-  lambda_function_arn     = module.lambda_function_container_image.lambda_function_arn
-  lambda_function_name    = var.lambda_function_name
-}
+# module "api_gateway" {
+#   source                  = "../terraform/modules/api_gateway"
+#   api_gateway_path_part   = local.api_gateway_path_part
+#   api_gateway_http_method = local.api_gateway_http_method
+#   api_gateway_stage_name  = local.api_gateway_stage_name
+#   lambda_function_arn     = module.lambda_function_container_image.lambda_function_arn
+#   lambda_function_name    = var.lambda_function_name
+# }
