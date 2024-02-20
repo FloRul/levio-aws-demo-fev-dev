f5e488b2bc81f1496becc6516e375ff969205229
Florian Rumiel <rumiel.florian@gmail.com>
2024-02-20 15:29:24 -0500

    Add print statement to display first document in retrieved list

--- lambdas/inference/src/index.py
+++ lambdas/inference/src/index.py
@@ -95,7 +95,7 @@ def lambda_handler(event, context):
                     relevance_treshold=ENV_VARS["relevance_treshold"],
                 )
                 docs = retrieval.fetch_documents(query=query, top_k=ENV_VARS["top_k"])
-
+                print(docs[0])
             if enable_history:
                 chat_history = json.loads(history.get(limit=10))
 
@@ -107,6 +107,7 @@ def lambda_handler(event, context):
                 history.add(
                     human_message=query, assistant_message=response, prompt=prompt
                 )
+
         result = {
             "completion": response,
             "docs": json.dumps(
