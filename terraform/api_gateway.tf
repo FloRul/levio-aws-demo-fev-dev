326f61277d8bf8fdb06625bd03be67f8cff25795
Florian Rumiel <rumiel.florian@gmail.com>
2024-02-25 08:50:21 -0500

    Fix provider_arns assignment in api_gateway.tf

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -56,7 +56,7 @@ resource "aws_api_gateway_authorizer" "this" {
   name          = "${var.api_name}-authorizer"
   rest_api_id   = aws_api_gateway_rest_api.this.id
   type          = "COGNITO_USER_POOLS"
-  provider_arns = [data.aws_cognito_user_pools.api_cognito.arns]
+  provider_arns = data.aws_cognito_user_pools.api_cognito.arns
 }
 
 
