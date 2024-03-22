51c3bc1c98dab2807a22000a634536d6ce63741c
brunoabreu-levio <bruno.abreu@levio.ca>
2024-03-22 12:20:09 -0400

    Response with form.

---
+++ lambdas/FormProcessor/FormRequestProcessorFunction/src/main/java/com/levio/awsdemo/formrequestprocessor/service/SqsProducerService.java
@@ -0,0 +1,38 @@
+package com.levio.awsdemo.formrequestprocessor.service;
+
+import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.MessageAttributeValue;
+import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SqsProducerService {
+    private static final String QUEUE_URL = System.getenv("QUEUE_URL");
+    private final SqsClient sqs = SqsClient.builder()
+            .region(Region.US_EAST_1)
+            .build();
+
+    public void send(String message, Map<String, SQSEvent.MessageAttribute> messageAttributes, String messageId) {
+        Map<String, MessageAttributeValue> messageAttributeValues = new HashMap<>();
+        messageAttributes.forEach((key, value) -> {
+            MessageAttributeValue mav = MessageAttributeValue.builder()
+                    .stringValue(value.getStringValue())
+                    .dataType(value.getDataType())
+                    .build();
+            messageAttributeValues.put(key, mav);
+        });
+
+        SendMessageRequest sendMessageRequest = SendMessageRequest.builder()
+                .queueUrl(QUEUE_URL)
+                .messageBody(message)
+                .messageAttributes(messageAttributeValues)
+                .messageGroupId(messageAttributes.get("sender").getStringValue())
+                .messageDeduplicationId(messageId)
+                .build();
+
+        sqs.sendMessage(sendMessageRequest);
+    }
+}
