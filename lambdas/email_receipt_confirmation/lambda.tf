07e3d8a86f4a2587ec3938a2c62627f391c79c24
FloRul <rumiel.florian@gmail.com>
2024-03-25 11:12:49 -0400

    Update email receipt confirmation lambda configuration

--- lambdas/email_receipt_confirmation/lambda.tf
+++ lambdas/email_receipt_confirmation/lambda.tf
@@ -1,12 +1,24 @@
 locals {
   lambda_function_name = "email-receipt-confirmation-dev"
   ses_arn              = "arn:aws:ses:us-east-1:446872271111:identity/lab.levio.cloud"
+  timeout              = 30
+  runtime              = "python3.11"
+  powertools_layer_arn = "arn:aws:lambda:${var.aws_region}:017000801446:layer:AWSLambdaPowertoolsPythonV2:67"
 }
 
 module "lambda_function_container_image" {
-  timeout                  = 30
-  source                   = "terraform-aws-modules/lambda/aws"
-  function_name            = local.lambda_function_name
+  source = "terraform-aws-modules/lambda/aws"
+
+  function_name = local.lambda_function_name
+  handler       = "index.lambda_handler"
+  publish = true
+
+  runtime = local.runtime
+  timeout = local.timeout
+  layers  = [local.powertools_layer_arn]
+
+  source_path = "${path.module}/src"
+
   memory_size              = 256
   role_name                = "${local.lambda_function_name}-role"
   attach_policy_statements = true
@@ -21,11 +33,12 @@ module "lambda_function_container_image" {
         "arn:aws:logs:*:*:*"
       ]
     }
+
     log_write = {
       effect = "Allow"
 
       resources = [
-        "arn:aws:logs:*:*:log-group:/aws/${loca.lambda_function_name}/*:*"
+        "arn:aws:logs:*:*:log-group:/aws/${local.lambda_function_name}/*:*"
       ]
 
       actions = [
--- lambdas/email_receipt_confirmation/src/index.py
+++ lambdas/email_receipt_confirmation/src/index.py
@@ -1,45 +1,56 @@
 import json
 import boto3
 import os
+from aws_lambda_powertools import Logger, Metrics
 
-ses = boto3.client('ses')
+logger = Logger()
+metrics = Metrics()
+
+ses = boto3.client("ses")
 
 
+@metrics.log_metrics
 def lambda_handler(event, context):
-    mail = json.loads(json.dumps(event['Records'][0]['ses']['mail']))
+    logger.info(event)
+    try:
+        mail = json.loads(json.dumps(event["Records"][0]["ses"]["mail"]))
 
-    subject = 'Re: ' + mail['commonHeaders']['subject']
+        subject = "Re: " + mail["commonHeaders"]["subject"]
 
-    source = mail['destination'][0]
+        source = mail["destination"][0]
 
-    to_addresses = [mail['source']]
+        to_addresses = [mail["source"]]
 
-    destination = {
-        'ToAddresses': to_addresses
-    }
+        destination = {"ToAddresses": to_addresses}
 
-    reply_text = os.environ.get(
-        "RECEIPT_REPLY_TEXT", "Nous avons reçu votre demande et elle sera traitée sous peu. Merci.\n"
-    )
+        reply_text = os.environ.get(
+            "RECEIPT_REPLY_TEXT",
+            "Nous avons reçu votre demande et elle sera traitée sous peu. Merci.\n",
+        )
 
-    reply_message = {
-        'Subject': {
-            'Data': subject,
-        },
-        'Body': {
-            'Text': {
-                'Data': reply_text
-            }
+        reply_message = {
+            "Subject": {
+                "Data": subject,
+            },
+            "Body": {"Text": {"Data": reply_text}},
         }
-    }
 
-    reply_to_addresses = [mail['destination'][0]]
+        reply_to_addresses = [mail["destination"][0]]
 
-    ses.send_email(
-        Destination=destination,
-        Message=reply_message,
-        ReplyToAddresses=reply_to_addresses,
-        Source=source
-    )
+        ses.send_email(
+            Destination=destination,
+            Message=reply_message,
+            ReplyToAddresses=reply_to_addresses,
+            Source=source,
+        )
 
-    return 'Email sent!'
+        return {
+            "statusCode": 200,
+            "body": json.dumps("Email sent successfully"),
+        }
+    except Exception as e:
+        logger.error(e)
+        return {
+            "statusCode": 500,
+            "body": json.dumps("An error occurred"),
+        }
