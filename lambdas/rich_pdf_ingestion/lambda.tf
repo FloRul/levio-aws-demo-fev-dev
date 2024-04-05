40e5cce7a76c970b048d027b3462c57e9ae0d4c1
Joel Balcaen <joelbalcaen@levio.ca>
2024-04-05 09:14:34 -0300

    remove layer

--- lambdas/rich_pdf_ingestion/lambda.tf
+++ lambdas/rich_pdf_ingestion/lambda.tf
@@ -2,7 +2,6 @@ locals {
   lambda_function_name = "rich_pdf_ingestion"
   ses_arn              = "arn:aws:ses:${var.aws_region}:${data.aws_caller_identity.current.account_id}"
   timeout              = 30
-  powertools_layer_arn = "arn:aws:lambda:${var.aws_region}:017000801446:layer:AWSLambdaPowertoolsPythonV2:67"
 }
 
 data "aws_caller_identity" "current" {}
@@ -18,7 +17,6 @@ module "lambda_function_container_image" {
   function_name            = local.lambda_function_name
   publish                  = true
   timeout                  = local.timeout
-  layers                   = [local.powertools_layer_arn]
   source_path              = "${path.module}/src"
   memory_size              = 256
   role_name                = "${local.lambda_function_name}-role"
