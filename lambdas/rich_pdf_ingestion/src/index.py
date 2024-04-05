0e308ebc3d644844ab3cf9726baeebedf8fe618c
Joel Balcaen <joelbalcaen@levio.ca>
2024-04-05 17:18:35 -0300

    bla

--- lambdas/rich_pdf_ingestion/src/index.py
+++ lambdas/rich_pdf_ingestion/src/index.py
@@ -2,7 +2,6 @@ import tabula
 import os
 import boto3
 from pypdf import PdfReader
-from botocore.exceptions import NoCredentialsError, BotoCoreError, ClientError
 
 OBJECT_CREATED = "ObjectCreated"
 EXTRACTED_TEXT_S3_OBJECT_KEY_PREFIX = 'pdf_extracted_text'
@@ -16,49 +15,38 @@ def generate_text_form_pdf(pdf_file_path):
     for page in reader.pages:
         text += page.extract_text()
 
-    dataFrames = tabula.read_pdf(pdf_file_path, pages="all",lattice=True)
+    dataFrames = tabula.read_pdf(pdf_file_path, pages="all", lattice=True)
     for df in dataFrames:
         text += df.to_html()
 
     return text
+
 
 def get_bucket_and_key(record):
     bucket = record["s3"]["bucket"]["name"]
     key = record["s3"]["object"]["key"]
     return bucket, key
 
+
 def fetch_file(bucket, key):
     local_filename = f"/tmp/{key.split('/')[-1]}"
-
-    try:
-        s3.download_file(bucket, key, local_filename)
-    except NoCredentialsError as e:
-        print(e)
-        raise e
-    except BotoCoreError as e:
-        print(e)
-        raise e
-    except ClientError as e:
-        print(e)
-        raise e
-    
+    s3.download_file(bucket, key, local_filename)
     return local_filename
+
 
 def upload_text(extracted_text, bucket, key):
     file_name = key.split('/')[-1].split('.')[0]+"_pdf_extracted_text.txt"
     local_file_path = "/tmp/"+file_name
-    s3_object_key = f"{EXTRACTED_TEXT_S3_OBJECT_KEY_PREFIX}/{file_name}"
+    s3_object_key = f"rfp/{EXTRACTED_TEXT_S3_OBJECT_KEY_PREFIX}/{file_name}"
 
     with open(local_file_path, "w") as f:
         f.write(extracted_text)
 
-    try:
-        s3.upload_file(local_file_path, bucket, s3_object_key)
-    except Exception as e:
-        print(e)
-        raise e
+    s3.upload_file(local_file_path, bucket, s3_object_key)
+    print(f"Stored file {s3_object_key} in bucket {bucket}")
 
-def lambda_handler(event, context): 
+
+def lambda_handler(event, context):
     print(event)
     records = event["Records"]
 
