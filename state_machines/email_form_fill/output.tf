c127345f80a4712a903f391f0ee960b69d9b3f0b
Joel Balcaen <joelbalcaen@levio.ca>
2024-04-17 17:09:34 -0300

    fixes

--- state_machines/email_form_fill/output.tf
+++ state_machines/email_form_fill/output.tf
@@ -1,7 +1,3 @@
-output "lambda_function_arn" {
-  value = module.lambda_function_container_image.lambda_function_arn
-}
-
-output "lambda_function_name" {
-  value = module.lambda_function_container_image.lambda_function_name
+output "state_machine_arn" {
+  value = module.aws_sfn_state_machine.arn
 }
