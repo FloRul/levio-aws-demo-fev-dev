3df5bd437147e6ac58a83db752494c0dcd9c8a3e
Joel Balcaen <jobalcaen+github@gmail.com>
2024-04-19 16:09:20 -0300

    return saved attachments as lambda response

--- lambdas/step_function_invoker/src/index.py
+++ lambdas/step_function_invoker/src/index.py
@@ -10,13 +10,13 @@ metrics = Metrics()
 
 step_functions_client = boto3.client("stepfunctions")
 STATE_MACHINE_ARN = os.environ.get("STATE_MACHINE_ARN")
-DISABLE_EVENT_PASS_THROUGH = os.environ.get("DISABLE_EVENT_PASS_THROUGH", False)
 
 def json_serializer(obj):
     """JSON serializer for objects not serializable by default json code"""
 
     if isinstance(obj, (datetime, date)):
         return obj.isoformat()
+    
     raise TypeError ("Type %s not serializable" % type(obj))
 
 
@@ -25,7 +25,6 @@ def lambda_handler(event, context):
     """
     This function is responsible for invoking the state machine with the given event.
     The state machine arn is defined in the environment variable STATE_MACHINE_ARN.
-    The state machine can be invoked without passing the event to the state machine by setting the environment variable DISABLE_EVENT_PASS_THROUGH to True.
     """
     logger.info(event)
 
@@ -33,7 +32,7 @@ def lambda_handler(event, context):
         state_machine_execution_result = step_functions_client.start_execution(
             stateMachineArn=STATE_MACHINE_ARN,
             # default to str to get around the datetime serialization issue
-            input = json.dumps(event, default=json_serializer) if not DISABLE_EVENT_PASS_THROUGH else {},
+            input = json.dumps(event, default=json_serializer)
         )
 
         logger.info(state_machine_execution_result)
@@ -42,26 +41,11 @@ def lambda_handler(event, context):
             "body": json.dumps(state_machine_execution_result),
         }
 
-    except ParamValidationError as e:
+    except (ParamValidationError, TypeError, Exception) as e:
         logger.error(e)
         return {
             "statusCode": 400,
-            "body": json.dumps({"error": "Parameter validation error", "details": str(e)}),
+            "body": str(e)
         }
-    
-    except TypeError as e:
-        logger.error(e)
-        return {
-            "statusCode": 400,
-            "body": json.dumps({"error": "Type error", "details": str(e)}),
-        }
-    
-    except Exception as e:
-        logger.error(e)
-        return {
-            "statusCode": 400,
-            "body": json.dumps(e),
-        }
-
 
 
