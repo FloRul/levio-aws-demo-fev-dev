b8706665d21b0e533433308f6e2102b57845fafd
Joel Balcaen <jobalcaen+github@gmail.com>
2024-04-19 11:53:45 -0300

    properly handle errors

--- lambdas/step_function_invoker/src/index.py
+++ lambdas/step_function_invoker/src/index.py
@@ -2,6 +2,7 @@ import json
 import boto3
 import os
 from aws_lambda_powertools import Logger, Metrics
+from botocore.exceptions import ParamValidationError
 
 logger = Logger()
 metrics = Metrics()
@@ -17,12 +18,18 @@ def lambda_handler(event, context):
     try:
         state_machine_execution_result = step_functions_client.start_execution(
             stateMachineArn=STATE_MACHINE_ARN,
-            input=json.dumps(event),
+            input=event,
         )
 
         return {
             "statusCode": 200,
             "body": json.dumps(state_machine_execution_result),
+        }
+    
+    except ParamValidationError as e:
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "Parameter validation error", "details": str(e)}),
         }
     
     except Exception as e:
