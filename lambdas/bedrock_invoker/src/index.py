a5505930997f44d595c9eb8cefdaf5e37448b8cf
Joel Balcaen <jobalcaen+github@gmail.com>
2024-04-25 12:21:41 -0300

    hardcode bedrock invoker values for now

--- lambdas/bedrock_invoker/src/index.py
+++ lambdas/bedrock_invoker/src/index.py
@@ -6,12 +6,12 @@ from urllib.parse import urlparse
 s3 = boto3.client('s3')
 bedrock = boto3.client('bedrock')
 
+
 def lambda_handler(event, context):
     """
     Invokes a bedrock model with the given parameters and s3 text object
     """
     s3_arn = event['s3_arn']
-    bedrock_params = event['bedrock_params']
     prompt = event['prompt']
 
     # Parse the S3 ARN to get the bucket and key
@@ -30,15 +30,28 @@ def lambda_handler(event, context):
     # Extract text from the S3 object
     extracted_text = s3_object['Body'].read().decode('utf-8')
 
+    claude_body = {
+        "anthropic_version": "bedrock-2023-05-31",
+        "max_tokens": 4096,
+        "system": "system promp",
+        "messages": [
+            {
+                "role": "user",
+                "content": {
+                    'type': "text",
+                    "text": +f"{prompt} <document>{extracted_text}</document>"
+                }
+            }
+        ]
+    }
+
     # Invoke the Bedrock model with the extracted text and the provided parameters
     try:
         response = bedrock.invoke_model(
-            ModelName=bedrock_params['model_name'],
-            Payload=json.dumps({
-                'master': bedrock_params['master'],
-                'prompt': prompt,
-                'message': extracted_text
-            })
+            body=json.dumps(claude_body),
+            contentType='application/json',
+            accept='application/json',
+            modelId='"anthropic.claude-3-sonnet-20240229-v1:0"',
         )
     except BotoCoreError as e:
         return {
