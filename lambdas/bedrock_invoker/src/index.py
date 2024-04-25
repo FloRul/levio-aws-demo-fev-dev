f41dd9cd8005996416554f41dac417d55890692a
Joel Balcaen <jobalcaen+github@gmail.com>
2024-04-25 16:42:41 -0300

    fix logging

--- lambdas/bedrock_invoker/src/index.py
+++ lambdas/bedrock_invoker/src/index.py
@@ -1,13 +1,11 @@
 import boto3
 import json
 from botocore.exceptions import BotoCoreError, ClientError
-from aws_lambda_powertools import Logger
 
 logger = Logger()
 s3 = boto3.client('s3')
 bedrock = boto3.client('bedrock-runtime')
 
-@logger.inject_lambda_context
 def lambda_handler(event, context):
     """
     Invokes a bedrock model with the given parameters and s3 text object
@@ -15,14 +13,14 @@ def lambda_handler(event, context):
     s3_arn = event['s3_arn']
     prompt = event['prompt']
 
-    logger.info(f"Invoking claude with prompt: ",prompt)
+    print(f"Invoking claude with prompt: ", prompt)
 
     # Parse the S3 ARN to get the bucket and key
     s3_path = s3_arn.replace("arn:aws:s3:::", "")
     bucket, key = s3_path.split('/', 1)
 
     # Download the file from S3
-    logger.info(f"Fetching file bucket: {bucket}, key: {key}")
+    print(f"Fetching file bucket: {bucket}, key: {key}")
     try:
         s3_object = s3.get_object(Bucket=bucket, Key=key)
     except ClientError as e:
@@ -33,7 +31,7 @@ def lambda_handler(event, context):
 
     # Extract text from the S3 object
     extracted_text = s3_object['Body'].read().decode('utf-8')
-    logger.info(f"Extracted text what is {len(extracted_text)} characters long")
+    print(f"Extracted text what is {len(extracted_text)} characters long")
     
     claude_body = {
         "anthropic_version": "bedrock-2023-05-31",
@@ -60,9 +58,9 @@ def lambda_handler(event, context):
             if 'text' in content:
                 content['text'] = "redacted"
 
-    logger.info(f"Invoke bedrock with this body: ", redacted_claude_body)
+    print(f"Invoke bedrock with this body: ", redacted_claude_body)
     bedrock_model = 'anthropic.claude-3-sonnet-20240229-v1:0'
-    logger.info(f"Invoke bedock with this model: ", bedrock_model)
+    print(f"Invoke bedock with this model: ", bedrock_model)
 
     # Invoke the Bedrock model with the extracted text and the provided parameters
     try:
