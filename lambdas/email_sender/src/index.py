43d65e2bcbafd9442e60306818b96ee7d55804d9
Joel Balcaen <jobalcaen+github@gmail.com>
2024-04-26 16:28:27 -0300

    refactor

--- lambdas/email_sender/src/index.py
+++ lambdas/email_sender/src/index.py
@@ -7,13 +7,13 @@ from email.mime.application import MIMEApplication
 def lambda_handler(event, context):
     """
     Downloads the attachment_s3_arns into memory and add them as attachments before sending the email with the given params.
-
+    attachment_s3_arns can be in URI or ARN format
     """
     sender_email = event['sender_email']
     destination_email = event['destination_email']
     subject = event['subject']
     body = event['body']
-    attachment_s3_uris = event['attachment_s3_uris']
+    attachment_s3_arns = event['attachment_s3_arns']
 
     client = boto3.client('ses')
     s3 = boto3.client('s3')
@@ -24,9 +24,9 @@ def lambda_handler(event, context):
     msg['To'] = destination_email
     msg.attach(MIMEText(body, 'plain'))
 
-    for attachment in attachment_s3_uris:
+    for attachment in attachment_s3_arns:
         try:
-            s3_bucket, s3_key = attachment.replace("s3://", "").split("/", 1)
+            s3_bucket, s3_key = attachment.replace("s3://", "").replace("arn:aws:s3:::", "").split("/", 1)
             file_obj = s3.get_object(Bucket=s3_bucket, Key=s3_key)
             file_content = file_obj['Body'].read()
 
