6fb7b4f91f17fd7f17cd78f2b71591ee03d3c79d
Joel Balcaen <jobalcaen+github@gmail.com>
2024-05-01 10:23:31 -0300

    increase timeout

--- lambdas/rich_pdf_ingestion/lambda.tf
+++ lambdas/rich_pdf_ingestion/lambda.tf
@@ -1,7 +1,7 @@
 locals {
   lambda_function_name = "rich_pdf_ingestion"
   ses_arn              = "arn:aws:ses:${var.aws_region}:${data.aws_caller_identity.current.account_id}"
-  timeout              = 300
+  timeout              = 600
 }
 
 data "aws_caller_identity" "current" {}
