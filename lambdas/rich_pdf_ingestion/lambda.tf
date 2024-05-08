d07517fa6aa300feb2d4141a881f119c794e5764
Joel Balcaen <jobalcaen+github@gmail.com>
2024-05-08 10:04:31 -0300

    increase pdf lambda timeout, add original input to send unknown email action

--- lambdas/rich_pdf_ingestion/lambda.tf
+++ lambdas/rich_pdf_ingestion/lambda.tf
@@ -1,7 +1,7 @@
 locals {
   lambda_function_name = "rich_pdf_ingestion"
   ses_arn              = "arn:aws:ses:${var.aws_region}:${data.aws_caller_identity.current.account_id}"
-  timeout              = 600
+  timeout              = 1200
 }
 
 data "aws_caller_identity" "current" {}
